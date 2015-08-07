431ba2971736b691139ec086dd68638261163166
Kit <dev_kit@icloud.com>
2015-08-06 19:44:36 -0700

    add event update and delete

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -12,39 +12,41 @@ class EventsController < ApplicationController
   end
 
   def new
-    @event = Event.new("name" => nil, "start_time" => nil, "end_time" => nil)
+    @event = Event.new("name" => nil,
+                       "start_time" => nil,
+                       "end_time" => nil,
+                       "contact" => { "name" => nil,
+                                      "phone" => nil,
+                                      "email" => nil
+    })
   end
 
   def create
     event = Event.create(params, nbuilder)
-    if event.code && event.code == "validation_failed"
-      flash[:warning] = "#{event.validation_errors.first}"
-      redirect_to :back
-    else
-      redirect_to event_path(event.event["id"])
-    end
+    redirect_to event_path(event.id)
   end
 
   def edit
     @pledge = Pledge.new
-    binding.pry
-    @contact = Contact.new(name: params[:id].to_i)
     @event  = Event.find(params[:id], nbuilder)
     @event.persisted = true
     @people = nbuilder.people if nation_token
   end
 
   def update
-    event = Event.update(params, nbuilder)
-    # if event.code == "validation_failed" && event.code
-    #   flash[:warning] = "#{event.validation_errors.first}"
-    #   redirect_to :back
-    # else
-      redirect_to event_path(params[:id])
-    # end
+    Event.update(params, nbuilder)
+    redirect_to event_path(params[:id])
+  end
+
+  def publish
+    event = Event.find(params[:id], nbuilder)
+    Event.publish(params, nbuilder)
+    redirect_to :back
   end
 
   def destroy
+    Event.destroy(params, nbuilder)
+    redirect_to "events/index"
   end
 
   def add_doners
