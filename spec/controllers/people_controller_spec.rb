fe14bd2078a177bb5f3fd58e888e5b174cc54b6f
Kit <dev_kit@icloud.com>
2015-08-10 11:57:03 -0700

    update css on home page

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe PeopleController, type: :controller do
 
   describe "GET #show" do
-    xit "returns http success" do
+    it "returns http success" do
       get :show
       expect(response).to have_http_status(:success)
     end
